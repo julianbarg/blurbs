8940f44776ace1ba68b6b72353908a826ada7d55
Julian Barg <barg.julian@gmail.com>
2021-02-12 09:31:37 -0500

    Changed get_big.sh to use project name.

--- reliability_validity/get_bib.sh
+++ reliability_validity/get_bib.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl http://127.0.0.1:23119/better-bibtex/export/collection?/1/7MX7MP2M.bibtex -o reliability_validity/bibliography.bib
+curl http://127.0.0.1:23119/better-bibtex/export/collection?/1/drafts/reliability_validity.bibtex -o reliability_validity/bibliography.bib
 
