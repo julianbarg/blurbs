fa2a9a25bf8a448ef0e9700034088e064c0c2ede
Julian Barg <barg.julian@gmail.com>
2021-02-13 15:53:39 -0500

    Implemented all changes for Zotero use.

--- 2020-02-04/get_bib.sh
+++ 2020-02-04/get_bib.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl http://127.0.0.1:23119/better-bibtex/export/collection?/1/drafts/blurb_2020-02-04.biblatex -o 2020-02-04/bibliography.bib
+curl http://127.0.0.1:23119/better-bibtex/export/collection?/1/drafts/blurb_2020-02-04.bibtex -o 2020-02-04/bibliography.bib
